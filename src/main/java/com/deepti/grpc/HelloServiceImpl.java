d7fcfbc7143e057448e7204745ff593e3832b1bc
Deepti Mittal <deepti@Deeptis-MacBook-Pro.local>
2023-07-03 10:50:41 +0530

    Adding APIs with big responses 100KB, 500KB, 1 MB, 5 MB

--- src/main/java/com/deepti/grpc/HelloServiceImpl.java
+++ src/main/java/com/deepti/grpc/HelloServiceImpl.java
@@ -1,8 +1,15 @@
 
 package com.deepti.grpc;
 
+import com.google.common.io.Resources;
+import com.google.protobuf.Empty;
 import io.grpc.stub.StreamObserver;
-    public class HelloServiceImpl extends HelloServiceGrpc.HelloServiceImplBase {
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class HelloServiceImpl extends HelloServiceGrpc.HelloServiceImplBase {
 
     @Override
     public void sayHello(
@@ -15,11 +22,58 @@ import io.grpc.stub.StreamObserver;
                 .append(request.getLastName())
                 .toString();
 
-        HelloResponse response = HelloResponse.newBuilder().setGreeting("Deepti")
+        HelloResponse response = HelloResponse.newBuilder().setGreeting(greeting)
                 .build();
 
         responseObserver.onNext(response);
         responseObserver.onCompleted();
+    }
+
+    @Override
+    public void grpc100KB(Empty par,
+                                 StreamObserver<BigResponse> responseObserver) {
+        BigResponse response = BigResponse.newBuilder().setResponse(getFileContents("ResponseFile/Response.txt"))
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void grpc500KB(Empty par,
+                          StreamObserver<BigResponse> responseObserver) {
+        BigResponse response = BigResponse.newBuilder().setResponse(getFileContents("ResponseFile/Response_1.txt"))
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void grpc1MB(Empty par,
+                          StreamObserver<BigResponse> responseObserver) {
+        BigResponse response = BigResponse.newBuilder().setResponse(getFileContents("ResponseFile/Response_2.txt"))
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void grpc5MB(Empty par,
+                          StreamObserver<BigResponse> responseObserver) {
+        BigResponse response = BigResponse.newBuilder().setResponse(getFileContents("ResponseFile/Response_3.txt"))
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    private static String getFileContents(String resourceName) {
+        URL url = Resources.getResource(resourceName);
+        String res;
+        try {
+            res = Resources.toString(url, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return res;
     }
     }
 
